78a4acd3a872f19240906a258155cdf241f41f4f
InveSte <32263484+investe@users.noreply.github.com>
2018-02-22 14:49:29 +0100

    Update register.php

--- register.php
+++ register.php
@@ -1,10 +1,6 @@
 <?php
 /* Database credentials. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
-define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'uomjat36_allerta');
-define('DB_PASSWORD', 'password');
-define('DB_NAME', 'uomjat36_allerta');
  
 /* Attempt to connect to MySQL database */
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
